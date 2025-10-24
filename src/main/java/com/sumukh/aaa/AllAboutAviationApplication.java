b713e34296dccaaf362d57ab82f832753fa44f65
Sumukh Bhandarkar <sumukhbhandarkar@gmail.com>
2025-10-25 00:11:52 +0530

    First commit

---
+++ src/main/java/com/sumukh/aaa/AllAboutAviationApplication.java
@@ -0,0 +1,13 @@
+package com.sumukh.aaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllAboutAviationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllAboutAviationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumukh/aaa/AllAboutAviationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumukh.aaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllAboutAviationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
