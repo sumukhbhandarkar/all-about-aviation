7cb4efd49a61b48303f723c6d0d2b3c08607576a
Sumukh Bhandarkar <sumukhbhandarkar@gmail.com>
2025-10-25 03:04:33 +0530

    Allow codeshare to be empty

--- src/main/java/com/sumukh/aaa/dto/CreateAirlineDTO.java
+++ src/main/java/com/sumukh/aaa/dto/CreateAirlineDTO.java
@@ -5,5 +5,9 @@ import java.time.OffsetDateTime;
 import java.util.List;
 
 public record CreateAirlineDTO(
-  @NotBlank String name, @NotBlank String baseCountry, String logoUrl, List<String> codeshares
+        @NotBlank String name,
+        @NotBlank String baseCountry,
+        String logoUrl,
+        @Size(max = 50)
+        List<@Pattern(regexp = "^[A-Za-z0-9]{2,3}$") String> codeshares // may be null
 ) {}
